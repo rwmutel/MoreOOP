7ff56078abb20d0756a8f68884193148d5455f14
Roman <romanmutel@gmail.com>
2022-10-06 17:18:44 +0300

    commented main out

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,10 @@ import lotr.*;
 
 public class Main {
     public static void main(String[] args) {
-        CharacterFactory factory = new CharacterFactory();
-        GameManager game = new GameManager();
-        Character first = factory.createCharacter();
-        Character second = factory.createCharacter();
-        game.fight(first, second);
+//        CharacterFactory factory = new CharacterFactory();
+//        GameManager game = new GameManager();
+//        Character first = factory.createCharacter();
+//        Character second = factory.createCharacter();
+//        game.fight(first, second);
     }
 }
